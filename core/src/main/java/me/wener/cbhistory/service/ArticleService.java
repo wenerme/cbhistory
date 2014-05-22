c73a89bbb2f57d3f14b0079b5dac5b55ae1f3998
wener <wenermail@gmail.com>
2014-05-22 17:08:53 +0800

    impelement findAllByDateBetween

--- core/src/main/java/me/wener/cbhistory/service/ArticleService.java
+++ core/src/main/java/me/wener/cbhistory/service/ArticleService.java
@@ -1,7 +1,10 @@
 package me.wener.cbhistory.service;
 
+import java.util.List;
 import me.wener.cbhistory.domain.entity.Article;
+import org.joda.time.LocalDateTime;
 
 public interface ArticleService extends BasicService<Article, Long>
 {
+    List<Article> findAllByDateBetween(LocalDateTime start, LocalDateTime end);
 }
--- core/src/main/java/me/wener/cbhistory/util/Same.java
+++ core/src/main/java/me/wener/cbhistory/util/Same.java
@@ -1,7 +1,14 @@
 package me.wener.cbhistory.util;
 
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.google.common.collect.ObjectArrays;
+import com.google.common.primitives.Longs;
+import com.google.common.primitives.Primitives;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
+import java.util.Collections;
 import me.wener.cbhistory.util.json.DateTimeTypeConvert;
 import me.wener.cbhistory.util.json.InstantTypeConvert;
 import me.wener.cbhistory.util.json.LocalDateTimeTypeConvert;
@@ -9,7 +16,10 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.Instant;
 import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+import org.joda.time.format.DateTimeParser;
 import org.joda.time.format.ISODateTimeFormat;
 
 public class Same
@@ -22,7 +32,17 @@ public class Same
     {
         if (dateTimeFormatter == null)
         {
-            dateTimeFormatter = ISODateTimeFormat.dateTime().withZone(DateTimeZone.UTC);
+            DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+            ArrayList<DateTimeParser> parser = Lists.newArrayList();
+            parser.add(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSSSSS").getParser());
+            parser.add(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm").getParser());
+            parser.add(DateTimeFormat.forPattern("yyyy-MM-dd").getParser());
+            parser.add(ISODateTimeFormat.dateTime().getParser());
+
+            dateTimeFormatter = builder
+                    .append(ISODateTimeFormat.dateTime().getPrinter()
+                            , parser.toArray(new DateTimeParser[parser.size()]))
+                    .toFormatter();
         }
         return dateTimeFormatter;
     }
